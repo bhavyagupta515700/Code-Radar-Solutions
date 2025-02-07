bee6350ee0f7c7138ebeade73883326eb2c2c883
bhavyagupta515700 <196902090+bhavyagupta515700@users.noreply.github.com>
2025-02-07 15:34:41 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
-int main()
-{
-    int side1,side2,side3;
-    scanf("%d%d%d",&side1,&side2,&side3);
-    if(side1+side2+side3=180){
-        printf("Valid");
+#include <stdio.h>
+
+int main() {
+    float side1, side2, side3;
+
+    scanf("%f %f %f", &side1, &side2, &side3);
+
+    // Check if the sides form a valid triangle
+    if ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) {
+        printf("Valid\n");
+    } else {
+        printf("Invalid\n");
     }
-    else{
-        printf("Invalid")
-    }
+
     return 0;
 }
