e9d2e1d964196ce14cf0d9dd6848bbbdaf02aaf6
bhavyagupta515700 <196902090+bhavyagupta515700@users.noreply.github.com>
2025-02-07 14:21:12 +0530

    Solution to Check Even or Odd

---
+++ Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1;
+    scanf("%d",&a);
+    if(a%4==0){
+        printf("number is even",num1)
+        else
+        printf("number is odd",num1)
+    }
+    return 0;
+}
