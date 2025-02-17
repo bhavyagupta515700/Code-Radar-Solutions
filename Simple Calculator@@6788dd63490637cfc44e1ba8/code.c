5385ddda0efff12f825b91b73fe6aceaa40f81b1
bhavyagupta515700 <196902090+bhavyagupta515700@users.noreply.github.com>
2025-02-17 21:24:20 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,33 +2,30 @@
 
 int main() {
     char operator;
-    double num1, num2;
+    double num1, num2, result;
 
-    printf("Enter an operator (+, -, *, /): ");
     scanf("%c", &operator);
-    printf("Enter two operands: ");
+    
     scanf("%lf %lf", &num1, &num2);
 
-    switch (operator) {
-        case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
-            break;
-        case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
-            break;
-        case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
-            break;
-        case '/':
-            if (num2 != 0) {
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
-            } else {
-                printf("Division by zero is not allowed.\n");
-            }
-            break;
-        default:
-            printf("Invalid operator.\n");
-            break;
+    if (operator == '+') {
+        result = num1 + num2;
+        printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+    } else if (operator == '-') {
+        result = num1 - num2;
+        printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+    } else if (operator == '*') {
+        result = num1 * num2;
+        printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+    } else if (operator == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+            printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+        } else {
+            printf("Division by zero is not allowed.\n");
+        }
+    } else {
+        printf("error\n");
     }
 
     return 0;
