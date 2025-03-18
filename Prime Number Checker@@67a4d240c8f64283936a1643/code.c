9cccdb133a42a26f087953f2eb2eabe5aa8db8a0
bhavyagupta515700 <guptaroshni411941@gmail.com>
2025-03-18 18:19:24 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,8 @@
+// Your code here...
+function isPrime(number):
+    if number <= 1:
+        return 0
+    for i from 2 to square root of number:
+        if number modulo i == 0:
+            return 0
+    return 1
